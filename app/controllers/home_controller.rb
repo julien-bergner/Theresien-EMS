9a881a670de1ab495a916170d60dafe84bede662
julien.bergner <julien@bergner.fr>
2012-08-11 23:31:07 +0200

    Extracting of seat information and seat displaying added.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,14 @@ class HomeController < ApplicationController
   end
 
   def booking
-    respond_to do |format|
-      format.html
+    @table_and_seat_data = Hash.new
 
-      end
-
+    @prom_tables = PromTable.all
+    for prom_table in @prom_tables do
+      seats_at_table = Seat.find_all_by_prom_table_id(prom_table.id)
+      @table_and_seat_data.store(prom_table, seats_at_table)
+    end
 
   end
+
 end
