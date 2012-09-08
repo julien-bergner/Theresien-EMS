3ef77c68f2bfd9cd386d59749a28e421e93bf500
Nicolas Fricke <nicolas.fricke@student.hpi.uni-potsdam.de>
2012-09-09 00:48:40 +0200

    Tables and Seats added to Seed file

--- app/models/prom_table.rb
+++ app/models/prom_table.rb
@@ -1,4 +1,4 @@
 class PromTable < ActiveRecord::Base
-  attr_accessible :position, :caption, :seat_description, :price
+  attr_accessible :position, :caption, :seat_description, :price, :orientation
   has_many :seats
 end
--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,5 +1,5 @@
 class Seat < ActiveRecord::Base
-  attr_accessible :position, :caption, :prom_table_id, :price, :status, :order_id
+  attr_accessible :position, :caption, :prom_table_id, :prom_table, :price, :status, :order_id
   belongs_to :prom_table
   belongs_to :order
 end
