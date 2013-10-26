23c5fc4cdd506bf9063d6722ed6e06fb0f7584e1
Julien Bergner <julien@bergner.fr>
2013-10-27 00:34:22 +0200

    Complete new dance_training_registration view

--- app/models/dance_training_registration.rb
+++ app/models/dance_training_registration.rb
@@ -1,3 +1,6 @@
 class DanceTrainingRegistration < ActiveRecord::Base
-  attr_accessible :amount, :email, :first_name, :last_name
+  attr_accessible :amount, :email, :first_name, :last_name, :date
+
+  validates_presence_of :amount, :email, :first_name, :last_name, :date
+  validates_format_of :email, :with => /(^$|^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$)/i, :message => "Bitte geben Sie eine g&uuml;ltige E-Mail-Adresse ein."
 end
